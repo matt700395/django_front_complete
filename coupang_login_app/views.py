ce1aad349acaccb3b3928801256c44c86845c6ce
matt700395 <matt7003@korea.ac.kr>
2023-05-19 15:52:03 +0900

    page 연결 완료

--- coupang_login_app/views.py
+++ coupang_login_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'coupang_login_app/index.html')
+
+def login(request):
+    return render(request, 'coupang_login_app/login.html')
--- django_front/urls.py
+++ django_front/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from coupang_login_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+
 ]
